3f14cc28d49e5e36286cf78287adf80a5829baa1
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2020-11-11 22:50:29 -0600

    Update themesGradients.dart

--- thirty_1.0/thirty/lib/standards/themesGradients.dart
+++ thirty_1.0/thirty/lib/standards/themesGradients.dart
@@ -8,14 +8,34 @@ class ThemesGradients {
 
   //User interface: Text linear gradient
   Shader textLinearGradient(
-      BuildContext context, Color topLeftColor, Color bottomRightColor) {}
+      BuildContext context, Color topLeftColor, Color bottomRightColor) {
+    final Shader linearGradient =
+        LinearGradient(colors: <Color>[topLeftColor, bottomRightColor])
+            .createShader(Rect.fromLTWH(110.0, 110.0, 200.0, 70.0));
+    return linearGradient;
+  }
 
   //User interface: Body linear gradient
-  LinearGradient bodyLinearGradient(
-      BuildContext context, Color topLeftColor, Color bottomRightColor) {}
+  LinearGradient bodyLinearGradient(BuildContext context, Color topLeftColor,
+      Color bottomRightColor, bool isSmall) {
+    final LinearGradient linearGradient = LinearGradient(
+      begin: isSmall ? Alignment.centerLeft : Alignment.topLeft,
+      end: Alignment.bottomRight,
+      tileMode: TileMode.mirror,
+      colors: <Color>[topLeftColor, bottomRightColor],
+    );
+    return linearGradient;
+  }
 
   //User interface: Card linear gradient
   //I may make these a bit blurred
   LinearGradient cardLinearGradient(
-      BuildContext context, Color topLeftColor, Color bottomRightColor) {}
+      BuildContext context, Color topLeftColor, Color bottomRightColor) {
+    final LinearGradient linearGradient = LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      tileMode: TileMode.mirror,
+      colors: <Color>[topLeftColor, bottomRightColor],
+    );
+  }
 }
