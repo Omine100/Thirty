5bebb60fd701166a4677fc99ccdf6cb617f3efd6
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2020-11-11 23:01:31 -0600

    Update themes.dart
    I think these are all the theme components that I need so far and for a few steps in the future
    
    I decided that I wanted to get rid of the customSizes thing and just throw everything into the dimensions method
    - This is going to take a bit of testing and seeing what works, but I think that it will be better overall

--- thirty_1.0/thirty/lib/standards/themes.dart
+++ thirty_1.0/thirty/lib/standards/themes.dart
@@ -2,24 +2,80 @@ import 'package:flutter/material.dart';
 
 extension CustomColorScheme on ColorScheme {
   //Color get 'name' => const Color(0x'hexValue');
+  Color get interfaceStandardsBackButtonColor => const Color(0xFFFFFFFF);
+  Color get interfaceStandardsHeaderTextColor => const Color(0xFFFFFFFF);
 
+  Color get loginTitleColor => const Color(0xFFFFFFFF);
+  Color get loginProgressBackgroundColor => const Color(0xFFFFFFFF);
+  Color get loginTextInputColor => const Color(0xFFEEEEEE);
+  Color get loginSignInSignUpButtonColor => const Color(0xFFFFFFFF);
+  Color get loginSignInSignUpAlternateTextColor => const Color(0xFFFFFFFF);
+  Color get loginForgotPasswordButtonColor => const Color(0xFFFFFFFF);
+  Color get loginErrorMessageColor => const Color(0xFFF44336);
+
+  Color get forgotPasswordTitleColor => const Color(0xFFFFFFFF);
+  Color get forgotPasswordTextInputColor => const Color(0xFFEEEEEE);
+  Color get forgotPasswordGradientTopLeftColor => const Color(0xFFFF9359);
+  Color get forgotPasswordGradientBottomRightColor => const Color(0xFFed2b7a);
+  Color get forgotPasswordResetColor => const Color(0xFFFFFFFF);
 }
 
 extension CustomFontSizes on TextTheme {
   //double get 'name' => 'fontSizeValue';
+  double get loginTitleIsSignInFontSize => 65.0;
+  double get loginTitleIsSignInFalseFontSize => 40.0;
+  double get loginTextInputFontSize => 22.0;
+  double get loginSignInSignUpButtonText => 22.5;
+  double get loginSignInSignUpAlternateTextFontSize => 15.0;
+  double get loginForgotPasswordButtonFontSize => 15.0;
+  double get loginErrorMessageFontSize => 13.0;
 
+  double get forgotPaswordTitleFontSize => 37.0;
+  double get forgotPasswordTextInputFontSize => 22.0;
+  double get forgotPasswordResetFontSize => 24.0;
 }
 
 extension CustomFontWeights on Typography {
   //FontWeight get 'name' => 'fontWeightValue';
+  FontWeight get loginTitleFontWeight => FontWeight.w600;
+  FontWeight get loginSignInSignUpButtonFontWeight => FontWeight.w600;
+  FontWeight get loginSignInSignUpAlternateTextFontWeight => FontWeight.w600;
+  FontWeight get loginForgotPasswordButtonFontWeight => FontWeight.w400;
+  FontWeight get loginErrorMessageFontWeight => FontWeight.w300;
 
+  FontWeight get forgotPasswordTitleFontWeight => FontWeight.w600;
 }
 
 extension CustomDimensions on MaterialTapTargetSize {
   double dimension({String selection, bool isHeight}) {
     switch (selection) {
       //case 'name': return isHeight ? 'height' : 'width'; break;
+      case "interfaceStandardsParentCenterContainerDimension":
+        return isHeight ? null : 1.0;
+        break;
+      case "interfaceStandardsBackButtonIconDimension":
+        return isHeight ? 0.1 : 0.1;
+        break;
 
+      case "loginContainerDimension":
+        return isHeight ? 1.0 : null;
+        break;
+      case "loginProgressContainerDimension":
+        return isHeight ? 0.0 : 0.0;
+        break;
+      case "loginSignInSignUpButtonDimension":
+        return isHeight ? null : 0.375;
+        break;
+      case "loginErrorMessageDimension":
+        return isHeight ? 0.1 : 0.1;
+        break;
+
+      case "forgotPasswordContainerDimension":
+        return isHeight ? 1.0 : null;
+        break;
+      case "forgotPasswordSendButtonDimension":
+        return isHeight ? null : 1.0;
+        break;
     }
   }
 }
@@ -28,7 +84,25 @@ extension CustomPositions on MaterialTapTargetSize {
   double position({String selection, bool isTop}) {
     switch (selection) {
       //case 'name': return isTop? 'top' : 'left'; break;
+      case "loginSignInSignUpAlternateTextIsSignInPosition":
+        return isTop ? 0.495 : 0.8;
+        break;
+      case "loginSignInSignUpAlternateTextIsSignInFalsePosition":
+        return isTop ? 0.525 : 0.8;
+        break;
+      case "loginProgressPosition":
+        return isTop ? 0.8 : null;
+        break;
 
+      case "forgotPasswordTitlePosition":
+        return isTop ? 0.35 : 0.15;
+        break;
+      case "forgotPasswordBackButtonPosition":
+        return isTop ? 0.06 : 0.06;
+        break;
+      case "forgotPasswordSendButtonPosition":
+        return isTop ? 0.675 : null;
+        break;
     }
   }
 }
