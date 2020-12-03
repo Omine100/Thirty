24d79eb2f4ace434f1b24e37fcf57c3953416392
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2020-12-03 01:34:15 -0600

    Update interfaceStandards.dart

--- thirty_1.0/thirty/lib/standards/interfaceStandards.dart
+++ thirty_1.0/thirty/lib/standards/interfaceStandards.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intro_slider/slide_object.dart';
 
 import 'package:thirty/services/appLocalizations.dart';
