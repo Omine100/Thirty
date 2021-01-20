0eee894f914f897f95911892311c02fa97530fba
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2021-01-19 23:35:09 -0600

    Update welcome.dart
    I need to finish going through the build function.
    
    Then I can start working on the next few pages.

--- thirty_1.2/thirty/lib/pages/welcome.dart
+++ thirty_1.2/thirty/lib/pages/welcome.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:thirty/services/cloudFirestore.dart';
 import 'package:thirty/services/routeNavigation.dart';
+import 'package:thirty/standards/languageStandards.dart';
 import 'package:thirty/standards/themes.dart';
 import 'package:thirty/standards/gradientStandards.dart';
 import 'package:thirty/standards/animationStandards.dart';
@@ -25,15 +26,56 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   InterfaceStandards interfaceStandards = new InterfaceStandards();
   MethodStandards methodStandards = new MethodStandards();
 
-  //Variable initialization
-  bool isSignIn;
+  //User interface: Show sign in or sign up button
+  Widget showSignInSignUpButton(bool isSignIn, Shader linearGradient) {
+    return new Container(
+      height: themes.getDimension(
+          context, true, "welcomeSignInSignUpButtonDimension"),
+      width: themes.getDimension(
+          context, false, "welcomeSignInSignUpButtonDimension"),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(90.0),
+        color: themes.getColor(context,
+            isSignIn ? "welcomeSignInButtonColor" : "welcomeSignUpButtonColor"),
+      ),
+      child: interfaceStandards.parentCenter(
+          context,
+          Text(
+            isSignIn
+                ? getTranslated(context, "welcomeSignInButton")
+                : getTranslated(context, "welcomeSignUpButton"),
+            style: TextStyle(
+              foreground: isSignIn
+                  ? (Paint()
+                    ..color = themes.getColor(
+                        context, "welcomeSignInButtonTextColor"))
+                  : (Paint()..shader = linearGradient),
+              fontSize: Theme.of(context)
+                  .textTheme
+                  .welcomeSignInSignUpButtonTextFontSize,
+              fontWeight: Theme.of(context)
+                  .typography
+                  .welcomeSignInSignUpButtonTextFontWeight,
+            ),
+          )),
+    );
+  }
 
-  //User interface Welcome screen
+  //User interface: Welcome screen
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: Container(
-        color: Colors.white,
+        height: themes.getDimension(context, true, "welcomeContainerDimension"),
+        decoration: BoxDecoration(
+            gradient: gradientStandards.bodyLinearGradient(
+                context,
+                themes.getColor(context, "backgroundGradientTopRightColor"),
+                themes.getColor(context, "backgroundGradientBottomLeftColor"),
+                false)),
+        child: Stack(
+          children: <Widget>[],
+        ),
       ),
     );
   }
