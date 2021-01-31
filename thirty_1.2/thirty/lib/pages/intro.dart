479cc5a9889b4073ca59192902786af6fbdc34e9
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2021-01-31 17:08:54 -0600

    intro.dart update
    Maybe got this page done? Still have to do some testing

--- thirty_1.2/thirty/lib/pages/intro.dart
+++ thirty_1.2/thirty/lib/pages/intro.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:intro_slider/intro_slider.dart';
+import 'package:intro_slider/slide_object.dart';
+import 'package:intro_slider/dot_animation_enum.dart';
 
 import 'package:thirty/services/routeNavigation.dart';
 import 'package:thirty/services/cloudFirestore.dart';
@@ -25,13 +29,127 @@ class _IntroScreenState extends State<IntroScreen> {
   GradientStandards gradientStandards = new GradientStandards();
   InterfaceStandards interfaceStandards = new InterfaceStandards();
 
+  //Variable initialization
+  List<Slide> slides = new List<Slide>();
+
+  //Mechanics: Initial state
+  void initState() {
+    super.initState();
+    slideCreation();
+  }
+
+  //Mechanics: Slide creation
+  List<Slide> slideCreation() {
+    slides.add(newSlide(getTranslated(context, "introExerciseTitle"),
+        getTranslated(context, "introExerciseDescription")));
+    slides.add(newSlide(getTranslated(context, "introCalendarTitle"),
+        getTranslated(context, "introCalendarDescription")));
+    slides.add(newSlide(getTranslated(context, "introMedalTitle"),
+        getTranslated(context, "introMedalDescription")));
+    return slides;
+  }
+
+  //User interface: New slide
+  Slide newSlide(String title, String description) {
+    Slide slide = new Slide(
+      title: title,
+      styleTitle: TextStyle(
+          color: themes.getColor(context, "introTitleColor"),
+          fontSize: Theme.of(context).textTheme.introTitleFontSize,
+          fontWeight: Theme.of(context).typography.introTitleFontWeight),
+      description: description,
+      styleDescription: TextStyle(
+        color: themes.getColor(context, "introDescriptionColor"),
+        fontSize: Theme.of(context).textTheme.introDescriptionFontSize,
+        fontWeight: Theme.of(context).typography.introDescriptionFontWeight,
+      ),
+    );
+    return slide;
+  }
+
+  //Mechanics: Route to home screen
+  void onDonePress() async {
+    cloudFirestore.signIn(widget.email, widget.password);
+    routeNavigation.RouteHome(context, false);
+  }
+
+  //User interface: Show interface button
+  Widget showInterfaceButton(String type) {
+    return Container(
+      height:
+          themes.getDimension(context, true, "introInterfaceButtonDimension"),
+      width:
+          themes.getDimension(context, false, "introInterfaceButtonDimension"),
+      child: Icon(
+        type == "navigate_next" ? Icons.navigate_next : Icons.done,
+        color: themes.getColor(context, "introInterfaceButtonColor"),
+        size: themes.getDimension(
+            context, true, "introInterfaceButtonIconDimension"),
+      ),
+    );
+  }
+
+  //User interface: Show list and tabs
+  List<Widget> renderListCustomTabs() {
+    List<Widget> tabs = new List();
+    for (int i = 0; i < slides.length; i++) {
+      Slide currentSlide = slides[i];
+      tabs.add(Container(
+        decoration: BoxDecoration(
+            gradient: gradientStandards.bodyLinearGradient(
+                context,
+                themes.getColor(context, "backgroundGradientTopRightColor"),
+                themes.getColor(context, "backgroundGradientBottomLeftColor"),
+                false)),
+        width: double.infinity,
+        height: double.infinity,
+        margin:
+            EdgeInsets.only(bottom: 60.0, top: 35.0, left: 50.0, right: 50.0),
+        child: ListView(
+          children: <Widget>[
+            Text(
+              currentSlide.title,
+              style: currentSlide.styleTitle,
+              textAlign: TextAlign.center,
+            ),
+            i == 0
+                ? Lottie.asset('lib/assets/introExerciseAnimation.json',
+                    repeat: true)
+                : (i == 1
+                    ? Lottie.asset(
+                        'lib/assets/introCalendarAnimation.json',
+                        repeat: true,
+                      )
+                    : Lottie.asset('lib/assets/introMedalAnimation.json',
+                        repeat: true)),
+            Text(
+              currentSlide.description,
+              style: currentSlide.styleDescription,
+              textAlign: TextAlign.center,
+              maxLines: 5,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+      ));
+    }
+    return tabs;
+  }
+
   //User interface: Intro screen
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: Container(
-        child: Text("Testing"),
-      ),
+    return new IntroSlider(
+      slides: this.slides,
+      isShowSkipBtn: false,
+      renderNextBtn: this.showInterfaceButton("navigate_next"),
+      renderDoneBtn: this.showInterfaceButton("done"),
+      onDonePress: this.onDonePress,
+      colorDot: themes.getColor(context, "introDotcolor"),
+      sizeDot: themes.getDimension(context, true, "introDotDimension"),
+      typeDotAnimation: dotSliderAnimation.SIZE_TRANSITION,
+      listCustomTabs: this.renderListCustomTabs(),
+      shouldHideStatusBar: false,
     );
   }
 }
